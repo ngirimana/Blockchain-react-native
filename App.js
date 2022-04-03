15c7deb1dd8ff60d07e3f486440d938bea8b979b
ngirimana <chadrackngirimana@gmail.com>
2022-04-03 16:57:06 +0200

    nft market place

--- App.js
+++ App.js
@@ -1,35 +1,45 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import Home from './screens/Home';
-import Details from './screens/Details';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
 
 const Stack = createStackNavigator();
 
-const theme = {
-  ...DefaultTheme.colors,
-  background:'transparent'
-}
 const App = () => {
-  const { loaded } = useFonts({
+  const [loaded] = useFonts({
     InterBold: require("./assets/fonts/Inter-Bold.ttf"),
     InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
-    InterMediumBold: require("./assets/fonts/Inter-Medium.ttf"),
-    InterRegularBold: require("./assets/fonts/Inter-Regular.ttf"),
-    InterLightBold: require("./assets/fonts/Inter-Light.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
   });
+
   if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}
-      initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}/>
-
-        <Stack.Screen name="Details" component={Details}/>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
-   </NavigationContainer>
+    </NavigationContainer>
   );
-}
+};
+
 export default App;
-
-
